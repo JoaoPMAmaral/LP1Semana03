b9c066000bec6d39ceccedcadfe1b51e0975a9f1
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-05 15:21:28 +0000

    updated solution, added changestring and rightsize

---
+++ ChangeString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Frase: ");
+            string frase = Console.ReadLine();
+
+            Console.Write("Caracter: ");
+            string caracter = Console.ReadLine();
+
+            char character = char.Parse(caracter);
+
+            for(int i = 0; i < frase.Length; i++)
+            {
+                if(frase[i] == character)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(frase[i]);
+                }
+            }
+        }
+    }
+}
---
+++ RightSize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+                if(s.Length > 3 && s.Length < 8)
+                {
+                    Console.WriteLine(s);
+                }
+                else if(s.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
